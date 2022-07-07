09801d5d001bd79353f2afe4cc27abae172c6c57
Haktan Can Taşkıran <98088644+hctaskiran@users.noreply.github.com>
2022-07-07 20:13:05 +0300

    update

---
+++ Ders_2/integer_float.py
@@ -0,0 +1,47 @@
+sayi1 = 5 ** 100
+sayi2 = 22 / 7
+
+print(sayi1)
+print(sayi2)
+print(type(sayi1))
+print(type(sayi2))
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(16/5)
+print(16//5)
+print(51//4)
+print(3**4)
+print(5**3)
+print(abs(-2.16))
+
+sayi= 3.316945646
+print(round(sayi,3)) # ilk 3 ondalık basamağı yazılması
+print(round(sayi,4))
+
+print(3+5*6)
+print((3+2) * 4 + 3)
+
+print(3==3)
+
+sayi3 = 7
+sayi4 = 10
+sayi5 = 10
+print(sayi3 < sayi4)
+print(sayi3 != sayi4)
+print(sayi4 <= sayi5)
+
+a = 1
+b = a
+a = 5
+print(b)
+
+sayi6 = "100"
+sayi7 = 100
+print(type(sayi6))
+print(type(sayi7))
+print(sayi6 == sayi7)
+
+sayi8 = int(sayi6)
+print(sayi8 == sayi7)
